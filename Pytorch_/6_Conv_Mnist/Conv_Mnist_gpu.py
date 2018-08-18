da1affe208c9aa6b93302c3ad59f9d2670301b75
amir-jafari <amir.h.jafari@okstate.edu>
2018-08-18 17:45:15 -0400

    Updated

--- Pytorch_/6_Conv_Mnist/Conv_Mnist_gpu.py
+++ Pytorch_/6_Conv_Mnist/Conv_Mnist_gpu.py
@@ -65,7 +65,7 @@ for epoch in range(num_epochs):
 
         if (i + 1) % 100 == 0:
             print('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f'
-                  % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, loss.data[0]))
+                  % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, loss.item()))
 # -----------------------------------------------------------------------------------
 # Test the Model
 cnn.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
