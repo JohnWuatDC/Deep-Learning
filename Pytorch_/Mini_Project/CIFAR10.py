f803fcdf5f13f037547b60a5a2dc9fc0d7271476
amir-jafari <amir.h.jafari@okstate.edu>
2018-10-14 19:33:41 -0400

    Updated

--- Pytorch_/Mini_Project/CIFAR10.py
+++ Pytorch_/Mini_Project/CIFAR10.py
@@ -7,55 +7,48 @@ import numpy as np
 import torch.nn as nn
 from torch.autograd import Variable
 # --------------------------------------------------------------------------------------------
-# Hyper Parameters
-input_size = 1 * 28 * 28
-hidden_size = 500
-num_classes = 10
-num_epochs = 1
-batch_size = 1
-learning_rate = 0.001
+# Choose the right values for x.
+input_size = x
+hidden_size = x
+num_classes = x
+num_epochs = x
+batch_size = x
+learning_rate = x
 # --------------------------------------------------------------------------------------------
-
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 # --------------------------------------------------------------------------------------------
-# Cifar10 Dataset
 
-train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_set = torchvision.datasets.CIFAR10(root='./data_cifar', train=True, download=True, transform=transform)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data_cifar', train=False, download=True, transform=transform)
 
 test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+# Find the right classes name. Save it as a tuple of size 10.
+classes = ('', '', ...)
 # --------------------------------------------------------------------------------------------
-
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
-# get some random training images
 dataiter = iter(train_loader)
 images, labels = dataiter.next()
 
-
-
-# show images
 imshow(torchvision.utils.make_grid(images))
-# print labels
 print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 # --------------------------------------------------------------------------------------------
-# Neural Network Model (1 hidden layer)
+# Choose the right argument for xx
 class Net(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc1 = nn.Linear(xx, xx)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)
+        self.fc2 = nn.Linear(xx, xx)
 
     def forward(self, x):
         out = self.fc1(x)
@@ -63,26 +56,21 @@ class Net(nn.Module):
         out = self.fc2(out)
         return out
 # --------------------------------------------------------------------------------------------
-
-net = Net(input_size, hidden_size, num_classes)
-
+# Choose the right argument for x
+net = Net(x, x, x)
 # --------------------------------------------------------------------------------------------
-# Loss and Optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+# Choose the right argument for x
+criterion = x
+optimizer = x
 # --------------------------------------------------------------------------------------------
-# Train the Model
+# There is bug here find it and fix it
 for epoch in range(num_epochs):
     for i, data in enumerate(train_loader):
-        # Convert torch tensor to Variable
+
         images, labels = data
-        images= images.view(-1,3 * 28 * 28)
-
-        # wrap them in Variable
+        images= images.view(3,-1 * 28 * 28)
         images, labels = Variable(images), Variable(labels)
-
-        # Forward + Backward + Optimize
-        optimizer.zero_grad()  # zero the gradient buffer
+        optimizer.zero_grad()
         outputs = net(images)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -92,15 +80,15 @@ for epoch in range(num_epochs):
             print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'
                   % (epoch + 1, num_epochs, i + 1, len(train_set) // batch_size, loss.data[0]))
 # --------------------------------------------------------------------------------------------
-# Test the Model
+# There is bug here find it and fix it
 correct = 0
 total = 0
 for images, labels in test_loader:
-    images = Variable(images.view(-1, 28 * 28)).cuda()
+    images = Variable(images.view(-1, 3, 28 * 28)).cuda()
     outputs = net(images)
     _, predicted = torch.max(outputs.data, 1)
     total += labels.size(0)
-    correct += (predicted.cpu() == labels).sum()
+    correct += (predicted == labels)
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
 # --------------------------------------------------------------------------------------------
@@ -108,16 +96,15 @@ print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct
 _, predicted = torch.max(outputs.data, 1)
 print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 # --------------------------------------------------------------------------------------------
-
-
+# There is bug here find it and fix it
 class_correct = list(0. for i in range(10))
 class_total = list(0. for i in range(10))
 for data in test_loader:
     images, labels = data
-    images = Variable(images.view(-1,3* 32 * 32)).cuda()
+    images = Variable(images.view(-1,1* 32 * 32)).cuda()
     outputs = net(images)
     _, predicted = torch.max(outputs.data, 1)
-    c = (predicted.cpu().numpy() == labels).squeeze()
+    c = (predicted.cpu() == labels)
     for i in range(4):
         label = labels[i]
         class_correct[label] += c[i]
@@ -126,5 +113,5 @@ for data in test_loader:
 # --------------------------------------------------------------------------------------------
 for i in range(10):
     print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
-
 # --------------------------------------------------------------------------------------------
+torch.save(net.state_dict(), 'model.pkl')
