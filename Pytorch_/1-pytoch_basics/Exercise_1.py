4fa744425bd05635bb4f7aa687223efa5299ab47
amir-jafari <amir.h.jafari@okstate.edu>
2018-10-30 11:08:45 -0400

    Updated

--- Pytorch_/1-pytoch_basics/Exercise_1.py
+++ Pytorch_/1-pytoch_basics/Exercise_1.py
@@ -1,5 +1,5 @@
 # --------------------------------------------------------------------------
-# Q1:  Rewrite the "2_tensor_pytorch.py" file in Neural Network Design Bookour
+# Q1:  Rewrite the "2_tensor_pytorch.py" file in Neural Network Design Book,
 # Chapter 11 notation (Check the Summary Page) and find out
 # what are the differences. Explain your findings.
 
